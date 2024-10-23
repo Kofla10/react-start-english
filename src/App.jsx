8bbfac13d34bc0127dc98cf88e9c701eaa289831
kofla10 <juank_milo94@hotmail.com>
2024-10-22 22:27:10 -0500

    condition

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,55 @@ import CoreConcept from './components/CoreConcept';
 import Header from './components/Header';
 import TabButton from './components/TabButton';
 import {CORE_CONCEPTS} from './data/data';
+import {EXAMPLES} from './data/data';
 
 function App() {
-  const [selectTopict, setSelectTopict] = useState('Please click a button');
+
+  const [selectTopict, setSelectTopict] = useState('');
 
   const handleselect = (selectedButton)  => {
     setSelectTopict(selectedButton);
-    console.log('handle select ' + selectedButton);
   }
 
-  console.log('APP COMPONENT EXWCUTING');
+    // console.log(EXAMPLES[selectTopict])
+    // const {title, description, code} = EXAMPLES[selectTopict];
 
   return (
     <div>
       <Header/>
       <main>
-        <section id='core-concepts' className='#core-concepts' >
+        <section id = 'core-concepts' className = '#core-concepts' >
         <h2>Core Concepts!</h2>
         <ul>
           {
             CORE_CONCEPTS.map( data =>(
-              <CoreConcept key={data.id} {...data}/>
+              <CoreConcept key = {data.id} {...data}/>
             ))
           }
           {/* <CoreConcept img ='' title='Camilo' description = 'this is a test'/> */}
         </ul>
         </section>
 
-        <section id='examples'>
+        <section id = 'examples'>
           <h2>Examples</h2>
           <menu>
             {/* <TabButton test='camilo'>Components</TabButton> */}
-            <TabButton onSelect ={ () => handleselect('componente') } label='Componente'/>
-            <TabButton onSelect ={ () => handleselect('jsx') } label='JSX'/>
-            <TabButton onSelect ={ () => handleselect('props') } label='Props'/>
-            <TabButton onSelect ={ () => handleselect('state') } label='State'/>
+            <TabButton onSelect = { () => handleselect('components') } label = 'Componente'/>
+            <TabButton onSelect = { () => handleselect('jsx') } label        = 'JSX'/>
+            <TabButton onSelect = { () => handleselect('props') } label      = 'Props'/>
+            <TabButton onSelect = { () => handleselect('state') } label      = 'State'/>
           </menu>
+          {!selectTopict ? <p>Please select a topic.</p>: null}
+          {selectTopict ?<div className = 'tab-context'>
+            <h3>{EXAMPLES[selectTopict].title}</h3>
+            <p>{EXAMPLES[selectTopict].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectTopict].code}
+              </code>
+            </pre>
+          </div>:null}
         </section>
-        {selectTopict}
       </main>
     </div>
   );
